18e024b5237a7646bf885202af32996b5d026a0d
Taufik Budi Santosa <taufikbudis@gmail.com>
2023-08-18 04:34:37 +0800

    Tipe Data List Comprehension

--- dasar_sintaksis_list_comprehension.py
+++ dasar_sintaksis_list_comprehension.py
@@ -21,3 +21,31 @@ daftar_buku = ['1. Seven Habits', '2. How to Influence People', '3. First Things
 del daftar_buku[0::2] #START:END:STEP
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print('\nMembuat List Baru')
+daftar_buku = ['1. Seven Habits', '2. How to Influence People', '3. First Things First', '4. 4DX']
+daftar_buku_baru = daftar_buku[:]
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+print('\nMembuat List Baru')
+del daftar_buku[:]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nMembuat List Baru dengan comprehension: ganjil')
+daftar_buku = ['1. Seven Habits', '2. How to Influence People', '3. First Things First', '4. 4DX']
+daftar_buku_baru = daftar_buku[0::2]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nMembuat List Baru dengan comprehension: genap')
+daftar_buku = ['1. Seven Habits', '2. How to Influence People', '3. First Things First', '4. 4DX']
+daftar_buku_baru = daftar_buku[1::2]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print('\nMembuat List Baru dengan comprehension: buang diujung')
+daftar_buku = ['1. Seven Habits', '2. How to Influence People', '3. First Things First', '4. 4DX']
+daftar_buku_baru = daftar_buku[0:-1:2]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
